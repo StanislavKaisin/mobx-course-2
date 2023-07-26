0535178a9ad4ac7e87a959398961b8c2374462b1
Stanislav-Kaisin <140324345+Stanislav-Kaisin@users.noreply.github.com>
2023-07-26 19:50:21 +0300

    added mobx store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import store, { StoreContext } from "./stores/index.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
+  </React.StrictMode>
+);
