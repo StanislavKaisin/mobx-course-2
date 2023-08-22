34b74f93e6aae2cc83408772d80455765abdddf1
Stanislav-Kaisin <140324345+Stanislav-Kaisin@users.noreply.github.com>
2023-08-22 12:29:15 +0300

    re
    q
    :

--- src/Pokedex/PokemonList.tsx
+++ src/Pokedex/PokemonList.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useInfiniteQuery, useQuery } from "react-query";
 import PokemonListItemWrapper from "./PokemonListItem/PokemonListItemWrapper";
 import { listFetcher } from "../Api";
 import { observer } from "mobx-react-lite";
 import styles from "./PokemonList.module.css";
+import { useStore } from "../stores";
+import { Link } from "react-router-dom";
 
 const POKEMON_LIST_ROUTE =
   "https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0";
 
 const PokemonList = ({ filter }: any) => {
+  const {app} = useStore();
   const { data, isLoading, fetchNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: ["pokemon-list"],
     queryFn: ({ pageParam = 0 }) => {
@@ -24,14 +27,24 @@ const PokemonList = ({ filter }: any) => {
     },
   });
 
+  useEffect(() => {
+    window.scrollTo(0, app.scrollPositionY)
+  }, [app])
+  
+
+  const handlePokemonClick = () => {
+    app.handleScrollPositionChange(window.scrollY)
+  }
+
   return (
     <>
       {!isLoading &&
         data?.pages.map((d) =>
           d.results.filter(filter).map((e: any) => {
-            return <PokemonListItemWrapper name={e.name} {...e} />;
-          })
-        )}
+            // return <PokemonListItemWrapper name={e.name} {...e} />;
+           return ( <Link to={`/details/${e.name}`} className={styles['pokemon-link']} onClick={handlePokemonClick}>
+                      <PokemonListItemWrapper name={e.name} {...e} />
+                    </Link>)}))}
       {hasNextPage && (
         <button
           className={styles["load-more-btn"]}
--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -3,9 +3,13 @@ import { makeAutoObservable } from "mobx";
 const AppStore = () =>
   makeAutoObservable({
     searchQuery: "",
+    scrollPositionY: 0,
     handleSearchQueryChange(value: string) {
       this.searchQuery = value;
     },
+    handleScrollPositionChange(value: number) {
+      this.scrollPositionY = value;
+    }
   });
 
 export default AppStore;
